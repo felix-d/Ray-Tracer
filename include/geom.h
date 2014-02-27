a03077460957e0c50703d6841adb3786d555fb50
felix <felix.descoteaux@hotmail.com>
2014-02-27 18:54:01 -0500

    ajout de structures de fonctions dans geom.cpp

--- include/geom.h
+++ include/geom.h
@@ -26,6 +26,8 @@ class Sphere : public Geometry
 public:
 	Sphere(vec3 position, vec3 orientation, vec3 scaling, Material* mtl = new Material());
 
+	//const after a function declaration means that the function is not allowed to 
+	//change any class members(except ones that are marked mutable)
 	virtual std::unique_ptr<struct Intersection> intersect(const struct Ray& ray, decimal &currentdepth) const override;
 };
 
--- src/geom.cpp
+++ src/geom.cpp
@@ -2,3 +2,44 @@
 #include <basic_structs.h>
 #include <glm/gtx/euler_angles.hpp>
 
+
+Sphere::Sphere(vec3 position, vec3 orientation, vec3 scaling, Material* mtl = new Material())
+:Geometry(position,orientation,scaling,mtl){
+    //TODO implementer constructeur Sphere
+}
+
+std::unique_ptr<struct Intersection> Sphere::intersect(const struct Ray& ray, decimal &currentdepth) const{
+    //TODO implementer intersection entre une sphere et un rayon
+	return NULL;
+}
+
+Box::Box(vec3 position, vec3 orientation, vec3 scaling, Material* mtl = new Material())
+:Geometry(position, orientation, scaling, mtl){
+    //TODO implementer constructeur boite
+}
+
+std::unique_ptr<struct Intersection> Box::intersect(const struct Ray& ray, decimal &currentdepth) const{
+    //TODO implementer intersection entre boite et rayon.
+	return NULL;
+}
+
+Cylinder::Cylinder(vec3 position, vec3 orientation, vec3 scaling, Material* mtl = new Material())
+:Geometry(position, orientation, scaling, mtl){
+	//TODO implementer constructeur cylindre
+}
+
+std::unique_ptr<struct Intersection> Cylinder::intersect(const struct Ray& ray, decimal &currentdepth) const{
+	//TODO implementer intersection entre cylindre et rayon.
+	return NULL;
+}
+
+Cone::Cone(vec3 position, vec3 orientation, vec3 scaling, Material* mtl = new Material())
+:Geometry(position, orientation, scaling, mtl){
+	//TODO implementer constructeur cone
+}
+
+std::unique_ptr<struct Intersection> Cone::intersect(const struct Ray& ray, decimal &currentdepth) const{
+	//TODO implementer intersection entre cone et rayon.
+	return NULL;
+}
+
