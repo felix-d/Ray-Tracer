1f24472b7a569ebd0f7c6fab71e16370f5373705
felix <felix.descoteaux@hotmail.com>
2014-03-04 16:32:29 -0500

    premiere ebauche de cone finie

--- include/geom.h
+++ include/geom.h
@@ -2,6 +2,7 @@
 #include <main.h>
 #include <material.h>
 #include <array>
+#include "../vs12/rtbase/utils.h"
 
 class Geometry
 {
@@ -13,7 +14,7 @@ public:
 	// Passing currentdepth (the linear distance between the ray's origin and the previous closest object)
 	// lets us do just that. Moreover, the & lets us update that value straight in the intersect method.
 	virtual std::unique_ptr<struct Intersection> intersect(const struct Ray& ray, decimal &currentdepth) const abstract;
-
+	
 protected:
 	vec3 _position;
 	vec3 _orientation;
@@ -76,6 +77,7 @@ protected:
 	vec3 _q;
 	decimal _radius;
 	decimal _height;
+;
 };
 
 class Cone : public Geometry
@@ -84,4 +86,13 @@ public:
 	Cone(vec3 position, vec3 orientation, vec3 scaling, Material* mtl = new Material());
 
 	virtual std::unique_ptr<struct Intersection> intersect(const struct Ray& ray, decimal &currentdepth) const override;
+protected:
+	vec3 _base_center;
+	vec3 _apex;
+	vec3 _direction;
+	decimal _theta;
+	decimal _radius;
+	decimal _height;
+	
 };
+
