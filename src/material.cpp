4f394be143e2ebd11edd184ce3bba525c4467a2b
felix <felix.descoteaux@hotmail.com>
2014-03-08 20:53:06 -0500

    jai arranger la reflection

--- src/material.cpp
+++ src/material.cpp
@@ -108,80 +108,33 @@ vec3 MaterialCombined::shadeLight(const Intersection* isect, const Light* l, uin
 
 //
 vec3 MaterialReflective::shade(const Intersection* isect, uint8_t depth) const {
+	if (depth >= isect->scene->maxDepth()) return _color;
 	depth++;
-	//On peut ajouter un coefficient si on veut que le materiel reflechisse aussi la lumiere.
-	decimal coefficient = 0.11;
 	decimal bias = 1e-4;
 	const std::vector<std::unique_ptr<Light>>& lights = isect->scene->lights();
 	vec3 total_light(0.0);
-	vec3 shadow_ray_origin = isect->position + bias * isect->normal;
-	for (uint i = 0; i < lights.size(); i++){
-		vec3 temp_light;
-		bool inShadow = false;
-		bool checkForDropShadows = true;
-		vec3 shadow_ray_direction;
 
-		//Light type check
-		if (lights[i]->type >= lights[i]->NO_SHADOWS)
-			inShadow = false;
-		else{
-			if (lights[i]->directional())
-				shadow_ray_direction = glm::normalize(-lights.at(i)->positionOrDirection);
-			else
-				shadow_ray_direction = glm::normalize(lights.at(i)->positionOrDirection - shadow_ray_origin);
+	vec3 v = isect->ray.direction;
+	vec3 n = isect->normal;
+	vec3 r = v - n * 2.0 * dot(v, n);
+	Ray ray_reflex = Ray{ isect->position + bias*isect->normal, r };
 
-			//Shadow ray
-			Ray shadow_ray = Ray{ shadow_ray_origin, shadow_ray_direction };
-			if (isect->scene->trace(shadow_ray, 1) == nullptr)
-				inShadow = false;
-			else
-				inShadow = true;
-		}
-
-		//Si ce n'est pas dans l'ombre, sinon cest automatiquement noir
-		//il est possible de rajouter une couleur d'ombre au lieu en rajoutant
-		//un else
-		if (!inShadow){
-			//Si on a pas encore atteint la profondeur maximale
-			if (depth != isect->scene->maxDepth()){
-				vec3 v = isect->ray.direction;
-				vec3 n = isect->normal;
-				vec3 r = v - n * 2.0 * dot(v, n);
-				Ray ray_reflex = Ray{ isect->position + bias*isect->normal, r };
-
-				std::unique_ptr<Intersection> isect2 = isect->scene->trace(ray_reflex, depth);
-				if (isect2 != nullptr){
-					temp_light = _color* vec3(isect2->material->shade(isect2.get(), depth));
-				}
-				else temp_light = _color * isect->scene->background();
-			}
-			//sinon on attribue a lintersection la couleur de lobjet, tout simplement
-			else temp_light = _color;
-
-			if (lights[i]->directional()){
-
-				//std::cout << lambert << std::endl;
-				//if (lambert < 0) std::cout << lambert<<std::endl;
-				temp_light = coefficient * temp_light * lights[i]->color;
-			}
-			else {
-				double dist = glm::length(lights[i]->positionOrDirection - isect->position);
-				temp_light = coefficient * (temp_light)* ((lights[i]->color*pi()) / pow(dist, 2.0));
-			}
-			
-		}
-		total_light = (i == 0) ? temp_light : total_light *= temp_light;
+	std::unique_ptr<Intersection> isect2 = isect->scene->trace(ray_reflex, depth);
+	if (isect2 != nullptr){
+		total_light = _color* vec3(isect2->material->shade(isect2.get(), depth));
 	}
-	return total_light/(decimal)lights.size();
+	else total_light = _color * isect->scene->background();
+
+	return total_light;
 }
 
 
 vec3 MaterialRefractive::shade(const Intersection* isect, uint8_t depth) const {
 	depth++;
-	//early exit
-	decimal coefficient = 0.25;
-	const std::vector<std::unique_ptr<Light>>& lights = isect->scene->lights();
 	if (depth >= isect->scene->maxDepth())return _color;
+	//early exit
+	const std::vector<std::unique_ptr<Light>>& lights = isect->scene->lights();
+	
 
 	//initialization
 	vec3 total_light = vec3(0.0);
@@ -207,7 +160,7 @@ vec3 MaterialRefractive::shade(const Intersection* isect, uint8_t depth) const {
 
 	//attribution de la couleur
 	vec3 temp_light;
-	if (refract != nullptr && depth < isect->scene->maxDepth())
+	if (refract != nullptr)
 		temp_light =refract->material->shade(refract.get(), depth) * _color;
 	else
 		temp_light =_color;
