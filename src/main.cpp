a50b4797f013f4a1862032738a8217669d6d22ae
b-hmel <b.hamel27@gmail.com>
2014-03-09 17:06:17 -0400

    rien dimportant

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, const char* argv[])
 	/////////////////////////////////
 
 	std::string outfilename = "image.ppm";
-	std::string infilename = "../../scenes/combined.scn";
+	std::string infilename = "../../scenes/uvmat.scn";
 
 	uint width = 640;
 	uint height = 480;
